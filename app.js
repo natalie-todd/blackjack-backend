02f4b11f6f1a4f3c5a2f85f7f162037fe8a364c0
Natalie <natalie.todd15@gmail.com>
2018-09-18 13:00:31 -0600

    switching to FE for now. Imports at top should be good and should be ready to try and setup routing and the DB

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
+var cors = require('cors');
 const monk = require('monk');
-const morgan = require('mongo-morgan-ext');
-// const db =  monk('localhost:27017/test');
-const mongo = require('mongodb');
+const db =  monk('localhost:27017/test');
+// const morgan = require('mongo-morgan-ext');
+// const commentRoutes = require("./routes/comments");
+// const router = express.Router();
+// const mongo = require('mongodb');
 
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 app.use(bodyParser.json());
+
+// app.use(commentRoutes);
 
 app.listen(port)
     .on("error", console.error.bind(console))
