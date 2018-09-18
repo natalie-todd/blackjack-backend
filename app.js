df7fcdd7567b47cfb7b46e65d93424985643c4c4
Natalie <natalie.todd15@gmail.com>
2018-09-18 11:19:33 -0600

    need to double check use of morgan and bodyParser, and also figure out use for monk and mongo, then can start writing routes

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
-const app = new express();
+const app = express();
+const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
-const morgan = require('mongo-morgan-ext');
-const db =  monk('localhost:27017/test');
-const mongo = require('mongodb');
 const monk = require('monk');
+const morgan = require('mongo-morgan-ext');
+// const db =  monk('localhost:27017/test');
+const mongo = require('mongodb');
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+app.listen(port)
+    .on("error", console.error.bind(console))
+    .on("listening", console.log.bind(console, "Listening on " + port));
