250e10c33b8bd04cfc9fb61490aad34457f5fe7a
Natalie <natalie.todd15@gmail.com>
2018-09-20 09:59:59 -0600

    adding test data for josh and george

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 var cors = require('cors');
 const monk = require('monk');
 const morgan = require('morgan');
-const db =  monk('localhost:27017/test');
+const db = monk('localhost:27017/test');
 // const commentRoutes = require("./routes/comments");
 // const router = express.Router();
 // const mongo = require('mongodb');
@@ -18,16 +18,31 @@ app.use(morgan('dev'));
 const users = db.get('user');
 
 db.then(() => {
-    console.log("Connected correctly to server");
-   })
+  console.log("Connected correctly to server");
+})
 
 app.get('/', (req, res) => {
-    console.log('test test');
-    users.find()
+  console.log('test test');
+  users.insert([{
+    id: 1,
+    email: 'luvmykitty@gmail.com',
+    username: 'luvmykitty',
+    password: 'rosie1234',
+    total_winnings: 0,
+    total_games_won: 0
+  },
+  {
+    id: 2,
+    email: 'gbeanster@gmail.com',
+    username: 'gbeanster',
+    password: 'rusticpenguin',
+    total_winnings: 0,
+    total_games_won: 0
+  }])
     .then((result) => {
-        res.json(result);
+      console.log('removed');
     })
-}) 
+})
 // app.use(commentRoutes);
 
 app.use(notFound)
@@ -40,16 +55,16 @@ function notFound(req, res, next) {
     // Don't log less important (automatic) browser requests
     console.error('[404: Requested file not found] ', url)
   }
-  res.status(404).send({error: 'Url not found', status: 404, url})
+  res.status(404).send({ error: 'Url not found', status: 404, url })
 }
 
 // eslint-disable-next-line
 function errorHandler(err, req, res, next) {
   console.error('ERROR', err)
-  const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
-  res.status(500).send({error: err.message, stack, url: req.originalUrl})
+  const stack = process.env.NODE_ENV !== 'production' ? err.stack : undefined
+  res.status(500).send({ error: err.message, stack, url: req.originalUrl })
 }
 
 app.listen(port)
-    .on("error", console.error.bind(console))
-    .on("listening", console.log.bind(console, "Listening on " + port));
+  .on("error", console.error.bind(console))
+  .on("listening", console.log.bind(console, "Listening on " + port));
